9992841f7ca14d3bf11d463391a6e5570a885b29
Espen Klem <espen.klem@gmail.com>
2021-04-25 22:01:08 +0200

    Fully automated now working Needed to rewrite method of inputting text to trigger event keyup in the browser Two things needs to be in place for Playwright to be done * understand how to extract text from div (the output div), and possibly wait for it * requiring Tape and doing assertions on result

--- test/playwright.js
+++ test/playwright.js
@@ -1,32 +1,31 @@
-const { chromium } = require('playwright');
+const { firefox } = require('playwright');
 
 (async () => {
-  const browser = await chromium.launch({
+  const browser = await firefox.launch({
     headless: false
-  });
-  const context = await browser.newContext();
+  })
+  const context = await browser.newContext()
 
   // Open new page
-  const page = await context.newPage();
+  const page = await context.newPage()
 
   // Go to file:///Users/eklem/github_modules/batr/test/stopword/demo/index.html
-  await page.goto('file:///Users/eklem/github_modules/batr/test/stopword/demo/index.html');
+  await page.goto('file:///Users/eklem/github_modules/batr/test/stopword/demo/index.html')
 
   await page.screenshot({ path: 'screenshot-01.png' })
 
   // Click [placeholder="Add some text"]
-  await page.click('[placeholder="Add some text"]');
+  await page.click('[placeholder="Add some text"]')
 
   // Fill [placeholder="Add some text"]
-  await page.fill('[placeholder="Add some text"]', 'a string of text with some nice and interesting words ');
+  await page.type('#text', 'a string of text with some nice and interesting words')
 
   // Click text=string text nice interesting words
-  await page.click('text=string text nice interesting words');
+  await page.click('text=string text nice interesting words')
   await page.screenshot({ path: 'screenshot-02.png' })
 
   // ---------------------
-  await context.close();
-  await browser.close();
-})();
-
+  await context.close()
+  await browser.close()
+})()
 
