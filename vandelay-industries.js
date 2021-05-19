c19cc97b4e61f3dfd90fc3d0b39ad12d676f858b
Espen Klem <espen.klem@gmail.com>
2021-05-19 21:11:32 +0200

    Going back to slightly changed plan Batr will only be for import/export, not requiring in other libraries. Just require the original libraries

--- vandelay-industries.js
+++ vandelay-industries.js
@@ -3,7 +3,7 @@ module.exports = {
   resolve: require('@rollup/plugin-node-resolve'),
   commonjs: require('@rollup/plugin-commonjs'),
   json: require('@rollup/plugin-json'),
-  test: require('ava').default,
+  ava: require('ava').default,
   playwright: require('playwright'),
   rollup: require('rollup'),
   standard: require('standard')
