b20e7e223851ad38444caae7aece65be8938e078
eklem <espen.klem@gmail.com>
2026-03-03 09:26:29 +0100

    switching to ESM import/export

--- vandelay-industries.js
+++ vandelay-industries.js
@@ -1,11 +1,11 @@
 // Importer / exporter, but nothing really happening
-module.exports = {
-  resolve: require('@rollup/plugin-node-resolve'),
-  commonjs: require('@rollup/plugin-commonjs'),
-  json: require('@rollup/plugin-json'),
-  terser: require('@rollup/plugin-commonjs'),
-  license: require('rollup-plugin-license'),
-  playwright: require('playwright'),
-  rollup: require('rollup'),
-  standard: require('standard')
-}
+import { resolve } from '@rollup/plugin-node-resolve'
+import { commonjs } from '@rollup/plugin-commonjs'
+import { json } from '@rollup/plugin-json'
+import { terser } from '@rollup/plugin-terser'
+import { license } from 'rollup-plugin-license'
+import { playwright } from 'playwright'
+import { rollup } from 'rollup'
+import { standard } from 'standard'
+
+export { resolve, commonjs, json, terser, license, playwright, rollup, standard }
